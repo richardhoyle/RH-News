356828ff7649ee7125455d7d0681e7faf4f936a2
Richard Hoyle <richardhoyle900@gmail.com>
2022-12-12 17:21:30 +0000

    made suggested ammendments inc arr length test & 500 error handler

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,16 +3,18 @@ const request = require("supertest");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/index");
+const topics = require("../db/data/test-data/topics.js");
 afterAll(() => db.end());
 beforeEach(() => seed(testData));
 
 describe("app general test", () => {
   test("404 - non-existant routes", () => {
-      return request(app).get("/notaroute")
-          .expect(404)
-          .then(({ body: { msg } }) => {
-              expect (msg).toBe('Error, path not found...')
-          });
+    return request(app)
+      .get("/notaroute")
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Error, path not found...");
+      });
   });
 });
 
@@ -22,12 +24,37 @@ describe("GET /api/topics", () => {
       .get("/api/topics")
       .expect(200)
       .then(({ body: { topics } }) => {
-        expect(topics).toBeInstanceOf(Array);
+          expect(topics).toBeInstanceOf(Array);
+          expect(topics).toHaveLength(3)
         topics.forEach((topic) => {
           expect(topic).toEqual(
             expect.objectContaining({
               slug: expect.any(String),
               description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
+
+describe("GET /api/articles", () => {
+  test("responds with an array of article objects with various properties", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body: { articles } }) => {
+        expect(articles).toBeInstanceOf(Array);
+        articles.forEach((article) => {
+          expect(article).toEqual(
+            expect.objectContaining({
+              author: expect.any(String),
+              title: expect.any(String),
+              article_id: expect.any(Number),
+              topic: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              comment_count: expect.any(Number),
             })
           );
         });
--- controllers/controllers.errors.js
+++ controllers/controllers.errors.js
@@ -2,5 +2,9 @@ const handleError404 = (req, res, next) => {
     res.status(404).send({ msg: 'Error, path not found...' })
 };
 
+const handleError500 = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "Server Error!" });
+};
 
-module.exports = {handleError404}
+module.exports = {handleError404, handleError500}
