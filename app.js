937739cbeb90e969fdae5ed6c56ef870020d194d
Richard Hoyle <richardhoyle900@gmail.com>
2022-12-13 13:40:58 +0000

    added functionality to accept GET requests to articles - decending order result

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require('./controllers/controllers.topics');
+const { getArticles } = require("./controllers/controllers.articles");
 const { handleError404, handleError500 } = require("./controllers/controllers.errors");
 
 
-
 app.get('/api/topics', getTopics)
+app.get('/api/articles', getArticles)
 app.all('*', handleError404)
 
 app.use(handleError500);
